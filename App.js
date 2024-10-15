b71c52e06074881d2e876d80b69887857d376904
salemye-yene <115075324+salemye-yene@users.noreply.github.com>
2024-10-15 15:07:28 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import OrderDetails from './components/OrderDetails';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-10">
+      <OrderDetails />
+    </div>
+  );
+}
+
+export default App;
